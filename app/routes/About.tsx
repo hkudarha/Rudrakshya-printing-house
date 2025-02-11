2e108f9a4265ca954df0f3d8c3162ab83eb08217
hkudarha <hkudarha@gmail.com>
2025-02-11 16:58:36 +0530

    first commit

---
+++ app/routes/About.tsx
@@ -0,0 +1,50 @@
+import React from "react"
+import SubscribeUs from "../components/SubscribeUs"
+import ShopOurInsta from "../components/ShopOurInsta"
+
+
+ function About() {
+    return (
+        <div>
+            {/* banner image  */}
+            <div
+            className="relative bg-cover bg-center h-[40vh] flex items-center  justify-center rounded-xl "
+            style={{ backgroundImage: "url('https://i.pinimg.com/736x/72/65/0b/72650bd4c3bb1aaa061053d5a3684531.jpg')" }}
+            >
+                <div>
+                    <h1 className="text-4xl font-bold mb-2 text-white ">About Us</h1>  
+                    <p className="text-white">Home &gt; <span className="underline">About Us</span> </p>
+                </div>             
+            </div>
+
+             
+             
+            <div className="flex flex-col md:flex-row items-center justify-center p-6 gap-6">
+                {/* Left Side - Video */}
+                <div className="w-full md:w-1/2">
+                    <video className="w-full h-[80vh] rounded-lg shadow-lg " controls>
+                    <source src="https://cdn.pixabay.com/video/2016/06/27/3576-172488163_large.mp4" type="video/mp4" />
+                    Your browser does not support the video tag.
+                    </video>
+                </div>
+                
+                {/* Right Side - Text Content */}
+                <div className="w-full md:w-1/2 text-center md:text-left">
+                    <h2 className="text-2xl font-bold">Best Digital Store BasicStore</h2>
+                    <p className="mt-2 text-gray-700">
+                        Risus augue curabitur diam senectus congue velit et. Sed vitae metus nibh sit era. Nulla adipiscing pharetra pellentesque maecenas odio eros at. Et libero vulputate amet duis erat volutpat vitae eget. Sed vitae metus nibh sit era. Nulla adipiscing pharetra eros at.
+
+                        Nulla adipiscing pharetra pellentesque maecenas odio eros at. Et libero vulputate amet duis erat volutpat vitae eget. Quam libero etiam et in ac at quis. Risus augue curabitur diam senectus congue velit et.
+                    </p>
+                    <button className="bg-black uppercase mt-10 text-white px-6 py-2 rounded-full text-sm hover:bg-gray-500 transition">Go To shop</button>
+                </div>
+            </div>
+
+            
+
+            <SubscribeUs/>
+            <ShopOurInsta/>    
+        </div>
+    )
+}
+export default About
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,58 @@
-import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
-
-export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
-}
+// import type { Route } from "./+types/home";
+import SubscribeUs from "~/components/SubscribeUs";
+import ShopOurInsta from "~/components/ShopOurInsta";
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <>
+      <div
+            className="relative bg-cover bg-center h-[90vh] flex items-center rounded-xl"
+            style={{ backgroundImage: "url('https://i.pinimg.com/736x/5d/c0/d2/5dc0d2dec85b7369fde3c510901ed603.jpg')" }}
+            >
+                <div className="max-w-md ml-12 text-white text-start">
+                    <h1 className="text-4xl font-bold mb-2">Iphone 15 Series</h1>
+                    <p className="text-lg mb-4">
+                    Limited Stocks Available. Grab It Now! 
+                    </p>
+                    <button className="bg-white uppercase text-black px-6 py-2 rounded-full font-semibold hover:bg-gray-300 transition">
+                    Shop Collection
+                    </button>
+                </div>
+      </div>
+
+
+      <div className=" mt-10  mb-10 flex flex-col md:flex-row gap-6 p-6 justify-center">
+                {/* Left Card */}
+                <div
+                    className="relative w-full md:w-1/2 h-80 bg-cover bg-center rounded-2xl flex items-center justify-center p-6 text-white shadow-lg"
+                    style={{ backgroundImage: "url('https://i.pinimg.com/736x/f8/29/47/f829470fab972a74d2d0cb6a18b2bb1a.jpg')" }}
+                    >
+                    <div className=" p-4 rounded-lg text-start">
+                    <h2 className="text-2xl font-bold">20% Off On Laptops</h2>
+                    <p className="mt-2">Products On Sale For Limited Time Only.</p>
+                    <button className="bg-white uppercase text-black px-5 py-2 rounded-full mt-5 text-sm  hover:bg-gray-300 transition">Shop it now</button>
+                    </div>
+                </div>
+                
+                {/* Right Card */}
+                <div
+                    className="relative w-full md:w-1/2 h-80 bg-cover bg-center rounded-2xl flex items-center justify-center p-6 text-white shadow-lg"
+                    style={{ backgroundImage: "url('https://i.pinimg.com/736x/10/f2/59/10f259d86d58999d8dc2c0e19473d3e1.jpg')" }}
+                    >
+                    <div className="  p-4 rounded-lg text-start">
+                    <h2 className="text-2xl font-bold">Apple Collection</h2>
+                    <p className="mt-2">Get Your Best Apple Products.</p>
+                    <button className="bg-white uppercase text-black px-5 py-2 rounded-full mt-5 text-sm  hover:bg-gray-300 transition">Shop Collection</button>
+                    </div>
+                </div>
+      </div>
+
+      <SubscribeUs/>
+      <ShopOurInsta/>
+
+               
+    </>
+  );
 }
+
+
