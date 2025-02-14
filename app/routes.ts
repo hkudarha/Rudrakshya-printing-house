d24cca346f2c69933e2ce40922337eb90bc955f6
hkudarha <hkudarha@gmail.com>
2025-02-14 17:56:23 +0530

    route

--- app/routes.ts
+++ app/routes.ts
@@ -5,12 +5,12 @@ import { type RouteConfig,
     } from "@react-router/dev/routes";
 
 export default [
-  layout( "./components/Layout.tsx",[
-      route("/","./routes/Home.tsx"),
-      route("/about","./routes/About.tsx"),
-      route("/contact", "./routes/Contact.tsx"),
-      route("/shop", "./routes/Shop.tsx"),
-      route("/cart", "./routes/Cart.tsx"),
-      route("/checkout","./routes/checkout.tsx")
+  layout( "components/Layout.tsx",[
+      index("routes/Home.tsx"),
+      route("about","routes/About.tsx"),
+      route("contact", "routes/Contact.tsx"),
+      route("shop", "routes/Shop.tsx"),
+      route("cart", "routes/Cart.tsx"),
+      route("checkout","routes/checkout.tsx")
   ]),
 ] satisfies RouteConfig;
